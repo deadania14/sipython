8ebef55ffc950f4d011c778517ceeff83bc18add
deadania <deadania14@gmail.com>
2017-03-21 12:24:24 +0700

    sudah tambah image

--- poll/urls.py
+++ poll/urls.py
@@ -6,4 +6,6 @@ urlpatterns= [
     url(r'^$', views.index, name='index'),
     #/poll/helpme
     url(r'^help/$', views.helpme, name='helpme'),
+    #/detail_question
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='detail_question'),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Question
 
 def index(request):
     context={}
-    return render(request, 'poll/index.html',context) #biar muncul
+    questions_query = Question.objects.all()
+    context['questions'] = questions_query
+    return render(request, 'poll/index.html',context) #render biar index muncul
 def helpme(request):
     context={}
     return render(request, 'poll/helpme.html',context) #biar muncul
+def detail_question(request, question_id):
+    context={}
+    question= Question.objects.get(id=question_id)
+    context['question'] = question
+    return render(request, 'poll/detail_question.html',context) #biar muncul
